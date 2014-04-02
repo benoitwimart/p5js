f415176eaa1a440adf2e1b7bee4f8c9c02991361
Benoît Wimart <benoit@macbok.local>
2014-04-02 18:10:13 +0200

    start
    first version

---
+++ p5js.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Processing code directly in your web page | p5js
+Plugin URI:
+Description: use shortcode to highlight syntax and use processingjs
+syntax highlighter by lea verou http://prismjs.com/
+Version: 1.4.7
+Author: @benoitWimart 
+Author URI: https://twitter.com/BenoitWimart
+License: GPL2
+*/
+
+/* Registers the script tag in Wordpress */
+wp_enqueue_script('processing', plugins_url('processing.min.js', __FILE__), false, '1.4.7');
+wp_enqueue_script('prism', plugins_url('prism.js', __FILE__), false, '1');
+wp_enqueue_style( 'style-css', plugins_url('prism.css', __FILE__));
+
+/* for remove br and p */
+remove_filter( 'the_content', 'wpautop' );
+
+
+function get_flags($atts) {
+    $flags = array();
+    if (is_array($atts)) {
+        foreach ($atts as $key => $value) {
+            if ($value != '' && is_numeric($key)) {
+                array_push($flags, $value);
+            }
+        }
+    }
+    return $flags;
+}
+
+/* Main Code */
+function addp5js( $params, $content= "" ) {
+
+
+global $post;
+$id="pjs-".$post->ID;
+
+return
+  (!in_array('editor', get_flags($params))?'':'<pre><code class="language-javascript">'.$content.'</code></pre>').
+  (!in_array('canvas', get_flags($params))?'':'<script type="application/processing" data-processing-target="'.$id.'">'.$content.'</script>'.
+	'<canvas id="'.$id.'"> </canvas>');
+	}
+
+/* Generating the shortcode */
+add_shortcode("p5js", "addp5js");
+
+
+// Add buttons to html editor
+add_action('admin_print_footer_scripts','eg_quicktags');
+function eg_quicktags() {
+?>
+<script type="text/javascript" charset="utf-8">
+/* Adding Quicktag buttons to the editor Wordpress ver. 3.3 and above
+* - Button HTML ID (required)
+* - Button display, value="" attribute (required)
+* - Opening Tag (required)
+* - Closing Tag (required)
+* - Access key, accesskey="" attribute for the button (optional)
+* - Title, title="" attribute (optional)
+* - Priority/position on bar, 1-9 = first, 11-19 = second, 21-29 = third, etc. (optional)
+QTags.addButton( 'eg_paragraph', 'p', '<p>', '</p>', 'p' );
+*/
+QTags.addButton( 'eg_p5js', 'p5js','[p5js editor canvas]', '[/p5js]', '' );
+</script>
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Processing code directly in your web page | p5js ===
+Contributors: @benoitwimart
+Donate link:
+Tags: JavaScript, Processing, processingjs, processing-js, code
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+Processing code directly in your web page
+
+== Installation ==
+
+
+1. Install 
+2. Activate 
+3. Add shortcode like [p5js editor canvas]void setup(){}[/p5js] (button in html editor)
+
+== Frequently Asked Questions ==
+
+- none -
+
+== Screenshots ==
+
+- none -
+
+== Changelog ==
+
+= 1.4.7 = is Processingjs Version
